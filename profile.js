99038c149de4b5d15ad31fb3059c482993f38e1f
Hippolyte-Menou <81096103+Hippolyte-Menou@users.noreply.github.com>
2024-06-02 23:12:33 +0200

    Add files via upload

--- profile.js
+++ profile.js
@@ -1,31 +1,33 @@
-let centromeres = [
-	[0.48, 0.59], // Chromosome 1
-	[0.37, 0.4], // Chromosome 2
-	[0.45, 0.48], // Chromosome 3
-	[0.25, 0.28], // Chromosome 4
-	[0.25, 0.28], // Chromosome 5
-	[0.34, 0.37], // Chromosome 6
-	[0.36, 0.4], // Chromosome 7
-	[0.29, 0.33], // Chromosome 8
-	[0.27, 0.49], // Chromosome 9
-	[0.28, 0.32], // Chromosome 10
-	[0.38, 0.41], // Chromosome 11
-	[0.25, 0.29], // Chromosome 12
-	[0.0, 0.17], // Chromosome 13
-	[0.0, 0.19], // Chromosome 14
-	[0.0, 0.2], // Chromosome 15
-	[0.38, 0.52], // Chromosome 16
-	[0.27, 0.32], // Chromosome 17
-	[0.19, 0.24], // Chromosome 18
-	[0.41, 0.48], // Chromosome 19
-	[0.41, 0.48], // Chromosome 20
-	[0.0, 0.21], // Chromosome 21
-	[0.0, 0.34], // Chromosome 22
-	[0.37, 0.4], // Chromosome X
-	[0.48, 1.0], // Chromosome Y
-]
+let centromeres = {
+    "1": [0.48, 0.59],  // Chromosome 1
+    "2": [0.37, 0.4],   // Chromosome 2
+    "3": [0.45, 0.48],  // Chromosome 3
+    "4": [0.25, 0.28],  // Chromosome 4
+    "5": [0.25, 0.28],  // Chromosome 5
+    "6": [0.34, 0.37],  // Chromosome 6
+    "7": [0.36, 0.4],   // Chromosome 7
+    "8": [0.29, 0.33],  // Chromosome 8
+    "9": [0.27, 0.49],  // Chromosome 9
+    "10": [0.28, 0.32], // Chromosome 10
+    "11": [0.38, 0.41], // Chromosome 11
+    "12": [0.25, 0.29], // Chromosome 12
+    "13": [0.0, 0.17],  // Chromosome 13
+    "14": [0.0, 0.19],  // Chromosome 14
+    "15": [0.0, 0.2],   // Chromosome 15
+    "16": [0.38, 0.52], // Chromosome 16
+    "17": [0.27, 0.32], // Chromosome 17
+    "18": [0.19, 0.24], // Chromosome 18
+    "19": [0.41, 0.48], // Chromosome 19
+    "20": [0.41, 0.48], // Chromosome 20
+    "21": [0.0, 0.21],  // Chromosome 21
+    "22": [0.0, 0.34],  // Chromosome 22
+    "X": [0.37, 0.4],   // Chromosome X
+    "Y": [0.48, 1.0]    // Chromosome Y
+};
 
+let mos_lines
 let jitter_amount = 0.01
+
 
 
 class Profile {
@@ -33,6 +35,11 @@ class Profile {
     this.type = type
     this.chromosome = chromosome
     this.mosaicism_level = mosaicism_level
+    
+    this.B_fetus = []
+    this.Total_fetus = []
+    this.B_mother = []
+    this.Total_mother = []
     
     this.lines = [];
     this.points = [];
@@ -42,13 +49,13 @@ class Profile {
   }
 
   display() {
+    // Drawing SNP points
     push()
     stroke(0, 0, 255);
     strokeWeight(3)
-    
     for (let p of this.points) {
       // Exclude points within centromeres and short arms of acrocentric chromosomes
-      if (p.x < centromeres[this.chromosome - 1][0]*this.number_of_points || p.x > centromeres[this.chromosome - 1][1]*this.number_of_points){
+      if (p.x < centromeres[this.chromosome][0]*this.number_of_points || p.x > centromeres[this.chromosome][1]*this.number_of_points){
         let x = map(p.x, 0, this.number_of_points, left_of_graph, width - right_of_graph)
         let y = map(p.y, 0, 1, height - bottom_of_graph, top_of_graph);
         point(x, y);
@@ -56,26 +63,58 @@ class Profile {
     }
     pop()
     
+    // Drawing ticks and value on the right of graph
     push()
-    
     for (let l of this.lines){
       strokeWeight(3)
       let y = map(l, 0, 1, height - bottom_of_graph, top_of_graph);
       line(width-right_of_graph, y, width-right_of_graph+5, y); // ticks
       textAlign(CENTER, CENTER)
       strokeWeight(1)
-      text(l.toFixed(3), width - right_of_graph + 25, y);
+      text(l.toFixed(2), width - right_of_graph + 25, y); // Value on the right
     }
     pop()
-   
+    
+    let top_of_second_graph = height - bottom_of_graph + 200
+    // Drawing bottom graph
+    push()
+    textAlign(CENTER, CENTER)
+    let column_width = (width-right_of_graph-left_of_graph) / 10  // TODO fix column width
+    for (let mos of [0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1]){
+      mos_lines = this.get_line_values(this.B_fetus, this.Total_fetus, this.B_mother, this.Total_mother, mos)
+      let x = map(mos, 0, 1, left_of_graph, width - right_of_graph)
+      line(x, top_of_second_graph, x, height - bottom) // Vertical lines left of column
+      line(width - right_of_graph + column_width, top_of_second_graph, width - right_of_graph + column_width, height - bottom) // Vertical line extreme right TODO fix col
+
+      push()
+      textSize(24)
+      textStyle(BOLD)
+      text(`${mos*100}%`, x + column_width/2, top_of_second_graph - 40 ) // Percentage at the top
+      pop()
+      
+      /*push()
+      noFill()
+      stroke(0)
+      arc(x + column_width/2, height - bottom, column_width, column_width, 0, -PI) // half-cirle ?
+      pop()*/
+      
+      for (let l in mos_lines){
+        let y = map(mos_lines[l], 0, 1, height - bottom, top_of_second_graph)
+        line(x, y, x+column_width, y)
+        text(mos_lines[l].toFixed(2), x+column_width/2, y-10)
+      } 
+    }
+      
+    
+    
+    pop()
+    
+  
   }
 
   generatePoints() {
-    console.log("generatePoints")
     this.type = type_selector.value()
     this.mosaicism_level = mosaicism_selector.value() / 100
-    console.log(this.type)
-    console.log(this.mosaicism_level)
     if (this.type === "Allosomes") {
       this.generateAllosomes();
     } else if (this.type === "X chromosome (if XY fetus)") {
@@ -93,101 +132,73 @@ class Profile {
     } else if (this.type === "Paternal Deletion") {
       this.generatePaternalDeletion();
     }
-    console.log(this.points)
-    for (let i = 0; i < this.points.length; i++) {
-      this.points[i][1] = this.points[i][1] 
-    }
-    console.log(this.chromosome - 1)
-    console.log(centromeres[this.chromosome - 1])
+    
+    this.lines = this.get_line_values(this.B_fetus, this.Total_fetus, this.B_mother, this.Total_mother, this.mosaicism_level)
+    this.createPoints()
   }
 
   // Function to generate points for Allosomes
   generateAllosomes() {
-    
-    let B_fetus = [2, 2, 1, 1, 1, 0, 0]
-    let Total_fetus = [2, 2, 2, 2, 2, 2, 2]
-    let B_mother = [2, 1, 2, 1, 0, 1, 0]
-    let Total_mother = [2, 2, 2, 2, 2, 2, 2]
-
-    this.lines = this.get_line_values(B_fetus, Total_fetus, B_mother, Total_mother)
-    this.createPoints()
+    this.B_fetus = [2, 2, 1, 1, 1, 0, 0]
+    this.Total_fetus = [2, 2, 2, 2, 2, 2, 2]
+    this.B_mother = [2, 1, 2, 1, 0, 1, 0]
+    this.Total_mother = [2, 2, 2, 2, 2, 2, 2]
   }
 
   // Function to generate points for X chromosome (if XY fetus)
   generateXYFetus() {
-    let B_fetus = [1, 1, 0, 0]
-    let Total_fetus = [1, 1, 1, 1]
-    let B_mother = [2, 1, 1, 0]
-    let Total_mother = [2, 2, 2, 2]
-
-    this.lines = this.get_line_values(B_fetus, Total_fetus, B_mother, Total_mother)
-    this.createPoints()
+    this.B_fetus = [1, 1, 0, 0]
+    this.Total_fetus = [1, 1, 1, 1]
+    this.B_mother = [2, 1, 1, 0]
+    this.Total_mother = [2, 2, 2, 2]
   }
 
   // Function to generate points for Maternal Heterotrisomy
   generateMaternalHeterotrisomy() {
-    let B_fetus = [3, 2, 2, 1, 1, 0]
-    let Total_fetus = [3, 3, 3, 3, 3, 3]
-    let B_mother = [2, 2, 1, 1, 0, 0]
-    let Total_mother = [2, 2, 2, 2, 2, 2]
-
-    this.lines = this.get_line_values(B_fetus, Total_fetus, B_mother, Total_mother)
-    this.createPoints()
+    this.B_fetus = [3, 2, 2, 1, 1, 0]
+    this.Total_fetus = [3, 3, 3, 3, 3, 3]
+    this.B_mother = [2, 2, 1, 1, 0, 0]
+    this.Total_mother = [2, 2, 2, 2, 2, 2]
   }
 
   // Function to generate points for Maternal Isotrisomy
   generateMaternalIsotrisomy() {
-    let B_fetus = [3, 3, 2, 2, 1, 1, 0, 0]
-    let Total_fetus = [3, 3, 3, 3, 3, 3, 3, 3]
-    let B_mother = [2, 1, 2, 1, 1, 0, 1, 0]
-    let Total_mother = [2, 2, 2, 2, 2, 2, 2, 2]
-
-    this.lines = this.get_line_values(B_fetus, Total_fetus, B_mother, Total_mother)
-    this.createPoints()
+    this.B_fetus = [3, 3, 2, 2, 1, 1, 0, 0]
+    this.Total_fetus = [3, 3, 3, 3, 3, 3, 3, 3]
+    this.B_mother = [2, 1, 2, 1, 1, 0, 1, 0]
+    this.Total_mother = [2, 2, 2, 2, 2, 2, 2, 2]
   }
 
   // Function to generate points for Paternal Heterotrisomy
   generatePaternalHeterotrisomy() {
-    let B_fetus = [3, 3, 2, 2, 2, 1, 1, 1, 0, 0]
-    let Total_fetus = [3, 3, 3, 3, 3, 3, 3, 3, 3, 3]
-    let B_mother = [2, 1, 2, 1, 0, 2, 1, 0, 1, 0]
-    let Total_mother = [2, 2, 2, 2, 2, 2, 2, 2, 2, 2]
-
-    this.lines = this.get_line_values(B_fetus, Total_fetus, B_mother, Total_mother)
-    this.createPoints()
+    this.B_fetus = [3, 3, 2, 2, 2, 1, 1, 1, 0, 0]
+    this.Total_fetus = [3, 3, 3, 3, 3, 3, 3, 3, 3, 3]
+    this.B_mother = [2, 1, 2, 1, 0, 2, 1, 0, 1, 0]
+    this.Total_mother = [2, 2, 2, 2, 2, 2, 2, 2, 2, 2]
   }
 
   // Function to generate points for Paternal Isotrisomy
   generatePaternalIsotrisomy() {
-    let B_fetus = [3, 3, 2, 2, 1, 1, 0, 0]
-    let Total_fetus = [3, 3, 3, 3, 3, 3, 3, 3]
-    let B_mother = [2, 1, 1, 0, 2, 1, 1, 0]
-    let Total_mother = [2, 2, 2, 2, 2, 2, 2, 2]
-
-    this.lines = this.get_line_values(B_fetus, Total_fetus, B_mother, Total_mother)
-    this.createPoints()
+    this.B_fetus = [3, 3, 2, 2, 1, 1, 0, 0]
+    this.Total_fetus = [3, 3, 3, 3, 3, 3, 3, 3]
+    this.B_mother = [2, 1, 1, 0, 2, 1, 1, 0]
+    this.Total_mother = [2, 2, 2, 2, 2, 2, 2, 2]
   }
   
   // Function to generate points for Maternal Deletion
   generateMaternalDeletion() {
-    let B_fetus = [1, 1, 1, 0, 0, 0]
-    let Total_fetus = [1, 1, 1, 1, 1, 1]
-    let B_mother = [2, 1, 0, 2, 1, 0]
-    let Total_mother = [2, 2, 2, 2, 2, 2]
-
-    this.lines = this.get_line_values(B_fetus, Total_fetus, B_mother, Total_mother)
-    this.createPoints()
+    this.B_fetus = [1, 1, 1, 0, 0, 0]
+    this.Total_fetus = [1, 1, 1, 1, 1, 1]
+    this.B_mother = [2, 1, 0, 2, 1, 0]
+    this.Total_mother = [2, 2, 2, 2, 2, 2]
   }
 
   // Function to generate points for Paternal Deletion
   generatePaternalDeletion() {
-    let B_fetus = [1, 1, 0, 0]
-    let Total_fetus = [1, 1, 1, 1]
-    let B_mother = [2, 1, 1, 0]
-    let Total_mother = [2, 2, 2, 2]
-
-    this.lines = this.get_line_values(B_fetus, Total_fetus, B_mother, Total_mother)
-    this.createPoints()    
+    this.B_fetus = [1, 1, 0, 0]
+    this.Total_fetus = [1, 1, 1, 1]
+    this.B_mother = [2, 1, 1, 0]
+    this.Total_mother = [2, 2, 2, 2]
   }
   
   createPoints () {
@@ -201,13 +212,12 @@ class Profile {
     return this.points
   }
   
-  get_line_values (B_fetus, Total_fetus, B_mother, Total_mother){
+  get_line_values (B_fetus, Total_fetus, B_mother, Total_mother, mosaicism_level){
     let lines = []
     let n_lines = B_fetus.length
     for (let i = 0; i < n_lines; i++){
-      lines.push(((1-this.mosaicism_level) * B_fetus[i] + this.mosaicism_level * B_mother[i]) / ((1-this.mosaicism_level) * Total_fetus[i] + this.mosaicism_level * Total_mother[i]) )
+      lines.push(((1-mosaicism_level) * B_fetus[i] + mosaicism_level * B_mother[i]) / ((1-mosaicism_level) * Total_fetus[i] + mosaicism_level * Total_mother[i]) )
     }
-    console.log(lines)
     return lines
   }
 }
--- sketch.js
+++ sketch.js
@@ -6,9 +6,13 @@ let chromosome = 1;
 let chromosome_image;
 let chromosome_images;
 
+let bottom = 50
+
+let height_of_graph = 600
+
 let top_of_graph = 170;
 let left_of_graph = 70;
-let bottom_of_graph = 50;
+let bottom_of_graph = top_of_graph + height_of_graph;
 let right_of_graph = 150;
 
 let chromosome_height = 100;
@@ -46,16 +50,14 @@ function preload() {
 }
 
 function setup() {
-  let canvas = createCanvas(800, 600);
-  // img = createImg("Images/explanation.jpg", "")
-  // img.position(190, 50);
-  // img.size(200, 200);
-  // canvas.position(300, 50);
-  background(255);
-
+  let canvas = createCanvas(800, 1400);
+  canvas.position(100, 100)
   create_mosaicism_selector()
   create_case_selector()
   create_chromosome_selector()
+  
+  let sidebar_text = createP("Maternal contamination in amniotic fluid samples poses a significant challenge to prenatal SNP array analysis. Using an a priori approach based on calculated levels of AA, AB, BB (AAA, AAB, ABB, BBB) genotypes, we can determine the level of maternal contamination found on SNP-array")
+  sidebar_text.position(930, 130)
   
 }
 
@@ -63,15 +65,22 @@ function draw() {
   background(255);
   strokeWeight(1)
   
-  draw_scales ()
+  draw_scales()
   draw_chromosome()
   
-  profile.display();
+  write_selector_line()
+
+  profile.display()
+  
+  line(left_of_graph, height - bottom_of_graph + 30, width - right_of_graph + 60, height - bottom_of_graph + 30) // Line separating top graph and bottom graph
+  
+  write_second_graph()
+
 }
 
 function create_mosaicism_selector(){
   mosaicism_selector = createSelect();
-  mosaicism_selector.position(width/2 + 250, 10)
+  mosaicism_selector.position(620, 106)
   mosaicism_options = [0, 5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55, 60, 65, 70, 75, 80, 85, 90, 95, 100]
   for (let mosaicism_option of mosaicism_options){mosaicism_selector.option(mosaicism_option);}
   style_mosaic_selector()
@@ -80,18 +89,19 @@ function create_mosaicism_selector(){
 
 function create_chromosome_selector(){
   chromosome_selector = createSelect();
-  chromosome_selector.position(width - 50, 10)
+  chromosome_selector.position(830, 106)
   style_chromosome_selector()
   chromosome_options = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "X", "Y"]
 
   for (let chromosome_option of chromosome_options){chromosome_selector.option(chromosome_option)}
-  update_chromosome()
   chromosome_selector.changed(update_chromosome)
+  update_chromosome()
+
 }
 
 function create_case_selector(){
   type_selector = createSelect();
-  type_selector.position(width / 2 - 50, 10);
+  type_selector.position(230, 106);
   type_options = ["Allosomes", "X chromosome (if XY fetus)", "Maternal Heterotrisomy", "Maternal Isotrisomy", "Paternal Heterotrisomy", "Paternal Isotrisomy", "Maternal Deletion", "Paternal Deletion"]
   
   for (let type_option of type_options) {type_selector.option(type_option);}
@@ -106,11 +116,14 @@ function draw_scales(){
 }
 
 function draw_vertical_scale(){
+  push()
   stroke(0);
   // Vertical scale
   line(left_of_graph, top_of_graph, left_of_graph, height - bottom_of_graph); // Y-axis
-
+  pop()
+  
   push()
+  textSize(14)
   textAlign(CENTER, CENTER);
   let BAF_letters = ["BB", "AB", "AA"]
   for (let i of [0, 0.5, 1] ) {
@@ -126,8 +139,10 @@ function draw_vertical_scale(){
   translate(10, (height - bottom_of_graph - top_of_graph) / 2 + top_of_graph);
   rotate(-PI / 2);
   textAlign(CENTER, CENTER);
-  textSize(12);
+  textSize(24);
   text("Bi-allellic Frequency", 0, 0);
+  translate(-680, 0)
+  text("Bi-allellic Frequency", 0, 0); // Second graph
   pop();
 }
 
@@ -153,6 +168,38 @@ function draw_chromosome(){
   pop();
 }
 
+function write_selector_line(){
+  push()
+  textAlign(LEFT, CENTER)
+  textSize(18)
+  text("SNP profile of ", 20, 20)
+  text(" for a mosaïcism of ", 360, 20)
+  text("% of chromosome ", 580, 20)
+  pop()
+}
+
+function write_second_graph(){
+  push()
+  textAlign(CENTER, CENTER)
+  textSize(28)
+  text("For a given level of maternal contamination, \nwhere are the expected lines on SNP array", width / 2, height - bottom_of_graph + 70)
+  pop()
+  
+  push()
+  textAlign(CENTER, CENTER)
+  textStyle(BOLD)
+  textSize(20)
+  text("Maternal contamination", width / 2, height - bottom_of_graph + 130)
+  pop()
+  
+  push()
+  textAlign(CENTER, CENTER)
+  textStyle(ITALIC)
+  textSize(16)
+  text("Key of interpretation : for a SNP profile of allosomes and a maternal contamination of 30%, \n7 lines are expected at [0, 0.15, 0.35, 0.5, 0.65, 0.85, 1] on BAF graph ", width / 2, height - 25)
+  pop()
+}
+
 function update_chromosome(){
   profile.chromosome = chromosome_selector.value()
   chromosome_image = chromosome_images[profile.chromosome]
@@ -161,22 +208,25 @@ function update_chromosome(){
 
 function style_chromosome_selector(){
   chromosome_selector.style("border", "none")
-  chromosome_selector.style("border-bottom", "1px solid #009acd") 
+  chromosome_selector.style("border-bottom", "1px solid black") 
   chromosome_selector.style("background", "none")
-  chromosome_selector.style("color", "#009acd")
- 
+  chromosome_selector.style("color", "black")
+  chromosome_selector.style("font-size", "18px") 
+
 }
 
 function style_type_selector(){
   type_selector.style("border", "none")
   type_selector.style("border-bottom", "1px solid black")
   type_selector.style("background", "none")
- 
+  type_selector.style("font-size", "18px") 
 }
 
 function style_mosaic_selector(){
   mosaicism_selector.style("border", "none")
   mosaicism_selector.style("border-bottom", "1px solid black")
   mosaicism_selector.style("background", "none")
+  mosaicism_selector.style("font-size", "18px") 
+
  
 }
